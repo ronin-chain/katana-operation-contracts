0ad709dbb83bbc0079de980b93cc39e613182861
Thai Xuan Dang <dang.thai@skymavis.com>
2024-11-12 14:36:35 +0700

    feat: add a check by getting pool from the factory in katanaV3SwapCallback

--- src/aggregate-router/modules/katana/v3/V3SwapRouter.sol
+++ src/aggregate-router/modules/katana/v3/V3SwapRouter.sol
@@ -6,6 +6,7 @@ import { BytesLib } from "./BytesLib.sol";
 import { IKatanaGovernance } from "@katana/v3-contracts/external/interfaces/IKatanaGovernance.sol";
 import { SafeCast } from "@katana/v3-contracts/core/libraries/SafeCast.sol";
 import { IKatanaV3Pool } from "@katana/v3-contracts/core/interfaces/IKatanaV3Pool.sol";
+import { IKatanaV3Factory } from "@katana/v3-contracts/core/interfaces/IKatanaV3Factory.sol";
 import { IKatanaV3SwapCallback } from "@katana/v3-contracts/core/interfaces/callback/IKatanaV3SwapCallback.sol";
 import { Constants } from "../../../libraries/Constants.sol";
 import { Permit2Payments } from "../../Permit2Payments.sol";
@@ -48,6 +49,7 @@ abstract contract V3SwapRouter is KatanaImmutables, Permit2Payments, IKatanaV3Sw
     (address tokenIn, uint24 fee, address tokenOut) = path.decodeFirstPool();
 
     if (computePoolAddress(tokenIn, tokenOut, fee) != msg.sender) revert V3InvalidCaller();
+    if (IKatanaV3Factory(KATANA_V3_FACTORY).getPool(tokenIn, tokenOut, fee) != msg.sender) revert V3InvalidCaller();
 
     (bool isExactInput, uint256 amountToPay) =
       amount0Delta > 0 ? (tokenIn < tokenOut, uint256(amount0Delta)) : (tokenOut < tokenIn, uint256(amount1Delta));
